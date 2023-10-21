32f5afa35e5b2811743649ed6843e098206b7e6d
bennetrr <bennet@bennetr.me>
2023-10-21 23:29:44 +0200

    Added old content

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,24 +5,10 @@ import starlight from '@astrojs/starlight';
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'SoftRoboter',
-
+			title: 'Softroboter',
 			social: {
-				github: 'https://github.com/withastro/starlight',
-			},
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', link: '/guides/example/' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+				github: 'https://github.com/bennetrr/softroboter-docs',
+			}
+		})
+	]
 });
