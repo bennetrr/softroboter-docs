8fca5fadf0a332a559c8ad45052b205ffc18993d
bennetrr <bennet@bennetr.me>
2023-10-21 23:46:00 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,15 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://softroboter.docs.bennetr.me',
 	integrations: [
 		starlight({
 			title: 'Softroboter',
+      description: 'Documentation for the InLab Softroboter project',
+      logo: {
+        src: './public/favicon.svg'
+      },
+      favicon: 'favicon.svg',
 			social: {
 				github: 'https://github.com/bennetrr/softroboter-docs',
 			}
